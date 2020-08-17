06416a242d26cd295277bc3d56e8b3bb62e1c006
JahirulKaiyoumd <imdjahirulislam76@gmail.como.com>
2020-08-17 22:49:50 +0600

    First commit

--- script.js
+++ script.js
@@ -7,16 +7,35 @@ document.getElementById("search-btn").addEventListener("click", function () {
             //console.log(songTitle);
             for (let i = 0; i <10; i++) {
                 const element = data.data[i];
-                console.log(element);
-                item(element);    
+                const artistName = element.artist.name;
+                const titleOfSong = element.title;
+                
+                // console.log(element);
+                // console.log(artistName, titleOfSong);
+        document.getElementById('list').innerHTML+=`<p class="author lead"><strong>${titleOfSong}</strong> Album by <span>${artistName}</span> <button class="btn btn-success" id="lastBtn" onclick="getLyrics('${artistName}','${titleOfSong}')" >Get Lyrics</button></p> `
+                //item(element); 
+                //  getLyrics(artistName ,titleOfSong);
         }
             
         })
-    function item(element) {
+    
 
-        document.getElementById('list').innerHTML+=`<p class="author lead"><strong>${element.title}</strong> Album by <span>${element.artist.name}</span> <button class="btn btn-success"  >Get Lyrics</button></p> `
-            
-    }
 });
 
- 
+
+function getLyrics(artistName, titleOfSong) {
+    
+    console.log(artistName ,titleOfSong);
+    fetch(`https://api.lyrics.ovh/v1/${artistName}/${titleOfSong}`)
+        .then(response => response.json())
+        .then(song => {
+           // console.log(data.lyrics)
+            document.getElementById("songLyrics").innerHTML+=`<h2 class="text-success mb-4">Lyrics</h2>
+            <pre class="lyric text-white">${song.lyrics}</pre>`
+
+        })
+} 
+
+ //lyrics
+
+  
