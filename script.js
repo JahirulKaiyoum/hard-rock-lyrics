7753344f2c054e722dcf0b1f184fb596e3c81c23
JahirulKaiyoumd <imdjahirulislam76@gmail.como.com>
2020-08-17 23:55:30 +0600

    last commit

--- script.js
+++ script.js
@@ -4,21 +4,18 @@ document.getElementById("search-btn").addEventListener("click", function () {
     fetch(`https://api.lyrics.ovh/suggest/${songName}`)
         .then(response => response.json())
         .then(data => {  
-            //console.log(songTitle);
+           
             for (let i = 0; i <10; i++) {
                 const element = data.data[i];
                 const artistName = element.artist.name;
                 const titleOfSong = element.title;
                 
-                // console.log(element);
-                // console.log(artistName, titleOfSong);
-        document.getElementById('list').innerHTML+=`<p class="author lead"><strong>${titleOfSong}</strong> Album by <span>${artistName}</span> <button class="btn btn-success" id="lastBtn" onclick="getLyrics('${artistName}','${titleOfSong}')" >Get Lyrics</button></p> `
-                //item(element); 
-                //  getLyrics(artistName ,titleOfSong);
+                
+        document.getElementById('list').innerHTML+=`<div class="mx-auto" style="width: 500px;"> <p  style="border: 1px solid white;text-align:center;"  class="author lead"><strong>${titleOfSong}</strong> Album by <span>${artistName}</span> <button class="btn btn-success " id="lastBtn" onclick="getLyrics('${artistName}','${titleOfSong}')" >Get Lyrics</button></p> </div>`
+      
         }
             
         })
-    
 
 });
 
@@ -29,13 +26,13 @@ function getLyrics(artistName, titleOfSong) {
     fetch(`https://api.lyrics.ovh/v1/${artistName}/${titleOfSong}`)
         .then(response => response.json())
         .then(song => {
-           // console.log(data.lyrics)
+           
             document.getElementById("songLyrics").innerHTML+=`<h2 class="text-success mb-4">Lyrics</h2>
             <pre class="lyric text-white">${song.lyrics}</pre>`
 
         })
 } 
 
- //lyrics
+ 
 
   
