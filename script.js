81a806f2b3a3244937e75ece64d82d78434e4179
JahirulKaiyoumd <imdjahirulislam76@gmail.como.com>
2020-08-18 20:29:50 +0600

    last update

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 document.getElementById("search-btn").addEventListener("click", function () {
     const songName = document.getElementById("songName").value;
-// console.log(song);
+
     fetch(`https://api.lyrics.ovh/suggest/${songName}`)
         .then(response => response.json())
         .then(data => {  
-           
+
+          
             for (let i = 0; i <10; i++) {
                 const element = data.data[i];
                 const artistName = element.artist.name;
                 const titleOfSong = element.title;
+                const albumTitle = element.album.title;
+                const duration = element.duration;
+                  
+                document.getElementById('list').innerHTML +=`<div class="search-result col-md-12 mx-auto py-2">
+                                                                <div class="single-result d-flex justify-content-between row align-items-center p-3">
+                                                                    <div class="col-md-9">
+                                                                        <h4 class="lyrics-name">${titleOfSong} &#127926&#127926</h4>
+                                                                        <p class="author lead"> <strong>Artist Name</strong> : <span>${artistName}</span></p>
+                                                                        <p> <strong>Album Titile</strong> : ${albumTitle}</p>
+                                                                        <p><strong>Duration</strong>: ${duration} Second &#127911 </p>
+                                                                    </div>
+                                                                    <div class="col-md-3 text-md-right text-center">
+                                                                        <button class="btn btn-success" onclick="getLyrics('${artistName}','${titleOfSong}')">Get Lyrics</button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>`
                 
-                
-        document.getElementById('list').innerHTML+=`<div class="mx-auto" style="width: 500px;"> <p  style="border: 1px solid white;text-align:center;"  class="author lead"><strong>${titleOfSong}</strong> Album by <span>${artistName}</span> <button class="btn btn-success " id="lastBtn" onclick="getLyrics('${artistName}','${titleOfSong}')" >Get Lyrics</button></p> </div>`
-      
-        }
-            
+                                                            
+            }    
         })
 
 });
 
+        function getLyrics(artistName,titleOfSong) {
+            
+            fetch(`https://api.lyrics.ovh/v1/${artistName}/${titleOfSong}`)
+                .then(response => response.json())
+                .then(song => {
+                
+                    document.getElementById("songLyrics").innerHTML=`<h2 class="text-success mb-4">${titleOfSong} &#127925&#127925&#127925</h2>
+                    <pre class="lyric text-white">${song.lyrics}</pre>`
 
-function getLyrics(artistName, titleOfSong) {
-    
-    console.log(artistName ,titleOfSong);
-    fetch(`https://api.lyrics.ovh/v1/${artistName}/${titleOfSong}`)
-        .then(response => response.json())
-        .then(song => {
-           
-            document.getElementById("songLyrics").innerHTML+=`<h2 class="text-success mb-4">Lyrics</h2>
-            <pre class="lyric text-white">${song.lyrics}</pre>`
-
-        })
-} 
-
- 
-
-  
+                })
+                
+        } 
