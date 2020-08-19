24f90ec45810063ec01906730c2f1d8aa38c394e
JahirulKaiyoumd <imdjahirulislam76@gmail.como.com>
2020-08-19 11:44:46 +0600

    last commit

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.getElementById("search-btn").addEventListener("click", function () {
     const songName = document.getElementById("songName").value;
+    document.getElementById('list').innerHTML = "";
 
     fetch(`https://api.lyrics.ovh/suggest/${songName}`)
         .then(response => response.json())
         .then(data => {  
-
-          
+           
             for (let i = 0; i <10; i++) {
                 const element = data.data[i];
+                console.log(element);
                 const artistName = element.artist.name;
                 const titleOfSong = element.title;
                 const albumTitle = element.album.title;
@@ -16,13 +17,13 @@ document.getElementById("search-btn").addEventListener("click", function () {
                 document.getElementById('list').innerHTML +=`<div class="search-result col-md-12 mx-auto py-2">
                                                                 <div class="single-result d-flex justify-content-between row align-items-center p-3">
                                                                     <div class="col-md-9">
-                                                                        <h4 class="lyrics-name">${titleOfSong} &#127926&#127926</h4>
+                                                                        <h1 class="text-primary lyrics-name">${titleOfSong} &#127926&#127926</h1>
                                                                         <p class="author lead"> <strong>Artist Name</strong> : <span>${artistName}</span></p>
                                                                         <p> <strong>Album Titile</strong> : ${albumTitle}</p>
                                                                         <p><strong>Duration</strong>: ${duration} Second &#127911 </p>
                                                                     </div>
                                                                     <div class="col-md-3 text-md-right text-center">
-                                                                        <button class="btn btn-success" onclick="getLyrics('${artistName}','${titleOfSong}')">Get Lyrics</button>
+                                                                        <button class="btn btn-primary" onclick="getLyrics('${artistName}','${titleOfSong}')">Get Lyrics</button>
                                                                     </div>
                                                                 </div>
                                                             </div>`
@@ -30,6 +31,7 @@ document.getElementById("search-btn").addEventListener("click", function () {
                                                             
             }    
         })
+    document.getElementById("songName").value = "";
 
 });
 
@@ -43,7 +45,7 @@ document.getElementById("search-btn").addEventListener("click", function () {
                     }
                     else {
                         
-                    document.getElementById("songLyrics").innerHTML=`<h2 class="text-success mb-4">${titleOfSong} &#127925&#127925&#127925</h2>
+                    document.getElementById("songLyrics").innerHTML=`<h1 class="text-primary mb-4">${titleOfSong} By <span id="artist-name"> ${artistName} &#127925&#127925&#127925</h1>
                     <pre class="lyric text-white">${song.lyrics}</pre>`
                     }
                 
